450ce3998e260bcf11490bbca650354c6c3fd566
kichan <kichancho@gmail.com>
2024-01-18 08:33:57 +0900

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { TbClock2 } from "react-icons/tb";
+import { Link } from 'react-router-dom';
+import { GoGraph } from "react-icons/go";
+
+export default function Navbar() {
+  return (
+    <header className='flex justify-between border-b border-gray-300 p-2 font-semibold'>
+        <Link to='/' className='flex items-center text-4xl text-brand'>
+        <TbClock2/>
+        <h1>Focus To-Do</h1>
+        </Link>
+        <nav className='flex items-center gap-4'>
+          <Link to='/report' className='text-2xl'>
+            <GoGraph />
+          </Link>
+        <button>Login</button>
+        </nav>
+    </header>
+
+  )
+}
