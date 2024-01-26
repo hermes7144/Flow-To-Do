7ff2ab8b99a7c8d22bd07e67cad68342a67a716e
kichan <kichancho@gmail.com>
2024-01-26 21:22:08 +0900

    feat:현재날짜 얻는 공용함수 추가

---
+++ src/js/CommonFunction.js
@@ -0,0 +1,9 @@
+export function getDate() {
+  const currentDate = new Date();
+  const formattedDate = `${currentDate.getFullYear()}${(
+    currentDate.getMonth() + 1
+  )
+    .toString()
+    .padStart(2, '0')}${currentDate.getDate().toString().padStart(2, '0')}`;
+  return formattedDate;
+}
