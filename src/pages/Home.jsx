60893b47221613e0e3ae9e926e34b95d95d9e5ba
kichan <kichancho@gmail.com>
2024-02-06 22:12:57 +0900

    feat:이미지 파일 업로드

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,17 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import TodoList from './../components/TodoList';
 import Pomodoro from '../components/Pomodoro';
 import PomodoroDashBoard from '../components/PomodoroDashBoard';
-import { useAuthContext } from '../context/AuthContext';
 import useTodos from '../hooks/useTodos';
 import { getDate } from '../js/CommonFunction';
 import AddTodo from '../components/AddTodo';
-import { SyncLoader } from 'react-spinners';
 
 export default function Home() {
   const {
     productsQuery: { isLoading, error, data: todos },
   } = useTodos();
 
-  if (isLoading)
-    return (
-      <div className='flex items-center justify-center h-dvh'>
-        <SyncLoader color='#fe5a4a' />
-      </div>
-    );
+  if (isLoading) return <span>Loading...</span>;
 
   const activeTodo = todos.filter(
     (todo) => todo.status === 'active' && todo.deadline <= getDate()
