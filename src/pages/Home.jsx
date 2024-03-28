90886a866d49328f74ddda56b6a9c0ab420f6d2b
hermes7144 <kichancho@gmail.com>
2024-03-28 17:26:42 +0900

    feat:투두 홈 리팩토링

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,18 @@
-import React, { useEffect, useRef, useState } from 'react';
-import TodoList from './../components/TodoList';
+import React, { useState } from 'react';
 import Pomodoro from '../components/Pomodoro';
-import PomodoroDashBoard from '../components/PomodoroDashBoard';
-import useTodos from '../hooks/useTodos';
-import AddTodo from '../components/AddTodo';
-import Sidebar from '../components/Sidebar';
-import { getDeadline, getToday } from '../js/CommonFunction';
-import SidebarToggle from '../components/SidbarToggle';
-
-function isDeadlineInRange(deadline, start, end) {
-  return deadline >= start && deadline <= end;
-}
-
-function filterActiveTodos(category, todos) {
-  const deadline = getDeadline(category, true);
-  if (!deadline) return [];
-
-  return todos.filter((todo) => todo.status === 'active' && (category === '내일' ? todo.deadline === deadline : category === '다음 주' ? isDeadlineInRange(todo.deadline, deadline.start, deadline.end) : todo.deadline <= deadline));
-}
+import TodoHeader from '../components/TodoHeader';
+import Todo from '../components/Todo';
+import SidebarCont from '../components/SidebarCont';
+import TodoCont from '../components/TodoCont';
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(true);
   const [isHovered, setIsHovered] = useState(false);
   const [category, setCategory] = useState('오늘');
-  const side = useRef();
-
-  const {
-    productsQuery: { data: todos },
-  } = useTodos();
 
   const handleSheduleClick = (category) => {
     setCategory(category);
-    setIsHovered(false);
+    isHovered && setIsHovered(false);
   };
 
   const handleMouseEnter = () => {
@@ -42,47 +23,14 @@ export default function Home() {
 
   const toggleSidebar = () => setIsOpen((prev) => !prev);
 
-  const activeTodo = filterActiveTodos(category, todos);
-  const completedTodo = todos.filter((todo) => todo.status === 'completed' && todo.completedDate === getToday());
-
-  // 사이드바 외부 클릭시 닫히는 함수
-  const handleClose = async (e) => {
-    let sideArea = side.current;
-    let sideCildren = side.current.contains(e.target);
-    if (isHovered && (!sideArea || !sideCildren)) {
-      await setIsHovered(false);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('click', handleClose);
-    return () => {
-      window.removeEventListener('click', handleClose);
-    };
-  });
-
   return (
     <div className='flex'>
-      {/* desktop sidebar */}
-      {isOpen && (
-        <div className='hidden md:block'>
-          <Sidebar category={category} handleSheduleClick={handleSheduleClick} />
-        </div>
-      )}
-      {/* mobile sidebar */}
-      <div ref={side} className={`${isHovered ? 'absolute' : 'hidden'} z-10`}>
-        <Sidebar category={category} handleSheduleClick={handleSheduleClick} />
-      </div>
-      <div className='flex-1 p-4 relative bg-gray-100 overflow-y-auto' style={{ height: 'calc(100vh - 57px)' }}>
-        <div className='flex items-center gap-2'>
-          <SidebarToggle isOpen={isOpen} toggleSidebar={toggleSidebar} handleMouseEnter={handleMouseEnter} />
-          <span className='text-xl font-bold'>{category}</span>
-        </div>
-        <PomodoroDashBoard activeTodo={activeTodo} completedCount={completedTodo.length} thisWeek={['오늘', '이번 주'].includes(category)} />
-        <AddTodo category={category} />
-        <TodoList activeTodo={activeTodo} completedTodo={completedTodo} category={category} />
-        <Pomodoro />
-      </div>
+      <SidebarCont isOpen={isOpen} isHovered={isHovered} setIsHovered={setIsHovered} category={category} handleSheduleClick={handleSheduleClick} />
+      <TodoCont>
+        <TodoHeader isOpen={isOpen} toggleSidebar={toggleSidebar} handleMouseEnter={handleMouseEnter} category={category} />
+        <Todo category={category} />
+      </TodoCont>
+      <Pomodoro />
     </div>
   );
 }
