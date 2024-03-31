d302ddfab5c1731eda765d5a9e4f18bdc3e46f82
hermes7144 <kichancho@gmail.com>
2024-03-31 12:21:27 +0900

    feat:1.칸반 날짜 및 디자인 2. 칸반 업데이트 구현

--- src/components/KanbanItem.jsx
+++ src/components/KanbanItem.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
+import TodoDate from './TodoDate';
+import useTodos from '../hooks/useTodos';
+import { usePomodoroContext } from '../context/PomodoroContext';
+import { getToday } from '../js/CommonFunction';
 
 export default function KanbanItem({ todo, index }) {
+  const { runningTodo, setRunningTodo, setIsRunning } = usePomodoroContext();
+
+  const { updateTodo } = useTodos();
+
+  const handleUpdate = (todo) => {
+    updateTodo.mutate({
+      ...todo,
+      completedDate: todo.status === 'active' ? getToday() : '',
+      status: todo.status === 'active' ? 'completed' : 'active',
+    });
+
+    if (todo.status === 'active' && runningTodo?.id === todo.id) {
+      setRunningTodo(null);
+      setIsRunning(false);
+    }
+  };
+
   return (
     <Draggable draggableId={todo.id} index={index}>
       {(provided) => (
-        <div className='flex bg-white rounded-lg gap-2 m-1 p-1' ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-          <input type='checkbox' />
-          <div className='flex flex-col'>
-            <div>{todo.name}</div>
-            <span>{todo.deadline}</span>
+        <div className='flex bg-white rounded-lg gap-2 m-1 shadow-md' ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+          <div className='ml-1 mt-1'>
+            <input type='checkbox' onClick={() => handleUpdate(todo)} checked={todo.status === 'completed'} />
+          </div>
+          <div className='flex flex-col text-xs'>
+            <div className='outline-none w-full  overflow-ellipsis'>{todo.name}</div>
+            <TodoDate date={todo.deadline} />
           </div>
         </div>
       )}
--- src/components/KanbanList.jsx
+++ src/components/KanbanList.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import KanbanItem from './KanbanItem';
 import { Droppable } from 'react-beautiful-dnd';
+import TodoDate from './TodoDate';
 
 export default function KanbanList({ date, todos }) {
   return (
-    <li className='flex flex-col m-1 bg-slate-200 w-40 basis-40 '>
-      {date}
+    <li className='flex flex-col m-1 p-1 min-w-60 basis-60 text-sm'>
+      <TodoDate date={date} />
       <Droppable droppableId={date}>
         {(provided, snapshot) => (
-          <ul className={`flex flex-col min-h-16 ${snapshot.isDraggingOver ? 'bg-slate-500' : ''}`} ref={provided.innerRef} {...provided.droppableProps} isDraggingOver={snapshot.isDraggingOver}>
+          <ul className={`flex flex-col min-h-16 ${snapshot.isDraggingOver ? 'bg-gray-200 opacity-50' : ''}`} ref={provided.innerRef} {...provided.droppableProps} isDraggingOver={snapshot.isDraggingOver}>
             {todos.map((todo, index) => (
               <KanbanItem key={todo.id} todo={todo} index={index} />
             ))}
